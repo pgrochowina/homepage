f828d58e534601bf99462780e16a76b323e865c2
Przemysław Grochowina <pgrochowina@gmail.com>
2020-05-07 23:37:41 +0200

    innitial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const name='Konrad Walenrod';
+const age='24';
+
+//alert(`Cześć i czołem, nazywam się ${name}, mam ${age} lat/a.`);
+console.log(`Poprawka: dzień dobry, nazywam się ${name}, mam ${age} lat/a.`);
