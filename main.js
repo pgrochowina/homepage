87ac717b6c93e29e22bfee4c8c17bd384ba78382
Przemysław Grochowina <pgrochowina@gmail.com>
2020-05-08 19:40:30 +0200

    auto fill with JS added

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const name='Konrad Walenrod';
-const age='24';
+const name = "Konrad Walenrod";
+const age = "24";
 
 //alert(`Cześć i czołem, nazywam się ${name}, mam ${age} lat/a.`);
-console.log(`Poprawka: dzień dobry, nazywam się ${name}, mam ${age} lat/a.`);
+//console.log(`Poprawka: dzień dobry, nazywam się ${name}, mam ${age} lat/a.`);
+
+newAutoHeader = document.querySelector(".auto-fill-header");
+newAutoParagraph = document.querySelector(".auto-fill-pragraph");
+console.log(newAutoHeader);
+console.log(newAutoParagraph);
+newAutoHeader.innerHTML = '"Non Stop"- Brian W. Aldiss';
+newAutoParagraph.innerHTML =
+  "Plemię Roya Complaina żyje w Pokładach porośniętych wybujałą i dziką roślinnością, walcząc o przetrwanie w nieprzyjaznym świecie. Wrogami są inne plemiona ludzi, flora i fauna. Istnieją też dalsze zagrożenia – mityczni Ludzie z Dziobu, legendarni Giganci czy przerażający Obcy.";
+console.log(newAutoHeader);
+console.log(newAutoParagraph);
